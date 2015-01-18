ccc1cf70fdd4002309a14ab0b283c8c70acca8a4
Montse <mtschamorro@gmail.com>
2015-01-18 16:12:13 +0100

    añadido atributo de tipo ArrayList a la clase Club y modificados los dos metodos de esta clase

--- Club.java
+++ Club.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 /**
  * Store details of club memberships.
  * 
@@ -7,13 +8,15 @@
 public class Club
 {
     // Define any necessary fields here ...
-    
+    // Almacena los socios del club
+    private ArrayList<Membership> socios;
     /**
      * Constructor for objects of class Club
      */
     public Club()
     {
         // Initialise any fields here ...
+        socios = new ArrayList<Membership>();
         
     }
 
@@ -23,6 +26,7 @@ public class Club
      */
     public void join(Membership member)
     {
+        socios.add(member);
     }
 
     /**
@@ -31,6 +35,6 @@ public class Club
      */
     public int numberOfMembers()
     {
-        return 0;
+        return socios.size();
     }
 }
